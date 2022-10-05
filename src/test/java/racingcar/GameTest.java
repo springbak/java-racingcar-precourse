0e216aa1ca058f44b18d843c83a01d4dfeca79f5
springbak <bmpark0403@naver.com>
2022-10-05 21:17:45 +0900

    test(GameTest): 우승자동차들을 결정하는 기능테스트

--- src/test/java/racingcar/GameTest.java
+++ src/test/java/racingcar/GameTest.java
@@ -14,27 +14,40 @@ public class GameTest {
     private Car car1;
     private Car car2;
     private Car car3;
+    private Car car4;
 
     @BeforeEach
-    void initTest(){
-         car1 = new Car(1,"BOMI");
-         car2 = new Car(3,"SUBINI");
-         car3 = new Car(2,"DARAMI");
+    void initTest() {
+        car1 = new Car(1, "BOMI");
+        car2 = new Car(3, "SUBINI");
+        car3 = new Car(2, "DARAMI");
+        car4 = new Car(3, "SUJINI");
     }
+
     @DisplayName("List중 우승을 결정하는 기능")
     @Test
     void pickWinnerTest() {
 
         Game game = new Game();
-        assertThat(game.pickWinner(Arrays.asList(1,3,2))==1).isTrue();
+        assertThat(game.pickWinner(Arrays.asList(1, 3, 2)) == 1).isTrue();
 
     }
+
     @DisplayName("우승자동차를 결정하는 기능")
     @Test
     void pickWinnerCarTest() {
 
         Game game = new Game();
-        assertThat(game.pickWinnerCar(Arrays.asList(car1,car2,car3)).equals(car2)).isTrue();
+        assertThat(game.pickWinnerCar(Arrays.asList(car1, car2, car3)).equals(car2)).isTrue();
+
+    }
+
+    @DisplayName("우승자동차들을 결정하는 기능")
+    @Test
+    void pickWinnerCarsTest() {
+
+        Game game = new Game();
+        assertThat(game.pickWinnerCars(Arrays.asList(car1, car2, car3, car4)).equals(Arrays.asList(car2, car4))).isTrue();
 
     }
 }
