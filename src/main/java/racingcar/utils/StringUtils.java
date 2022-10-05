6cf5902466d0281127f3050dbf4c365c2765794f
springbak <bmpark0403@naver.com>
2022-10-05 23:30:49 +0900

    feat(StringUtils): 입력된 String값으로 이름들을 받는 기능

--- src/main/java/racingcar/utils/StringUtils.java
+++ src/main/java/racingcar/utils/StringUtils.java
@@ -1,10 +1,7 @@
 package racingcar.utils;
-
 import java.util.Arrays;
-
 public class StringUtils {
     public static final String SPLIT_EXPRESSION = ",";
-
     public String[] getNamesFromUserInput(String inputString) {
         String[] s = inputString.split(SPLIT_EXPRESSION);
         System.out.println(Arrays.toString(s));
