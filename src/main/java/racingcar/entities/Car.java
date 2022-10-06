d621db83003c5352a63b043130a8b05d62b38a4d
springbak <bmpark0403@naver.com>
2022-10-07 00:37:22 +0900

    feat(Move): car가 한 횟수의 게임을 진행하는 기능

--- src/main/java/racingcar/entities/Car.java
+++ src/main/java/racingcar/entities/Car.java
@@ -1,20 +1,28 @@
 package racingcar.entities;
 
+import racingcar.utils.Move;
+
 public class Car {
 
     public static final int INIT_DISTANCE = 0;
-    private final int distance;
+    public static final int RANDOM_NUMBER_START = 0;
+    public static final int RANDOM_NUMBER_END = 9;
+    private int distance;
     private final String name;
+    private int randomNumber;
+    private boolean isThisCarJustMoved;
 
 
     public Car(int distance, String name) {
         this.distance = distance;
         this.name = name;
+
     }
 
     public Car(String name) {
         this.name = name;
         this.distance = INIT_DISTANCE;
+        this.isThisCarJustMoved = false;
     }
 
 
@@ -28,10 +36,32 @@ public class Car {
 
 
     public int getFurtherDistance(int maxDistance) {
-        return Math.max(maxDistance,this.getDistance());
+        return Math.max(maxDistance, this.getDistance());
     }
 
     public boolean isCarWinner(int maxDistance) {
-        return maxDistance==this.getDistance();
+        return maxDistance == this.getDistance();
+    }
+
+    public void proceedGameOnce() {
+        Move move = new Move();
+        initIsThisCarJustMoved();
+        this.randomNumber = move.getRandomNumber(RANDOM_NUMBER_START, RANDOM_NUMBER_END);
+        if (move.isCarMove(this.randomNumber)) {
+            this.distance++;
+            this.isThisCarJustMoved = true;
+        }
+    }
+
+    public boolean isThisCarJustMoved() {
+        return isThisCarJustMoved;
+    }
+
+    private void initIsThisCarJustMoved() {
+        this.isThisCarJustMoved = false;
+    }
+
+    public int getRandomNumber() {
+        return randomNumber;
     }
 }
