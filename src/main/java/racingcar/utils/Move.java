8c7f19841bea120df0759cd4fd70302c2a79b403
springbak <bmpark0403@naver.com>
2022-10-07 22:35:40 +0900

    feat(Move): 움직인 차들을 식별하는 메서드

--- src/main/java/racingcar/utils/Move.java
+++ src/main/java/racingcar/utils/Move.java
@@ -1,5 +1,11 @@
 package racingcar.utils;
 
+import racingcar.entities.Car;
+
+import java.util.ArrayList;
+
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+
 public class Move {
 
     public static final int BASE_NUM = 4;
@@ -8,5 +14,19 @@ public class Move {
         if (number < BASE_NUM)
             return false;
         return true;
+    }
+    public int getRandomNumber(int start, int end) {
+        int randomNumber = pickNumberInRange(start,end);
+        return randomNumber;
+    }
+
+    public ArrayList<Car> getJustMovedCars(ArrayList<Car> cars) {
+        ArrayList<Car> movedCars = new ArrayList<>();
+        for(Car car : cars)
+            if(car.isThisCarJustMoved()) {
+
+                movedCars.add(car);
+            }
+        return movedCars;
     }
 }
