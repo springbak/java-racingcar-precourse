814c92f11324c700f025f0a902bde3d537a08f98
springbak <bmpark0403@naver.com>
2022-10-07 22:34:15 +0900

    test(MoveTest): 한 게임이 끝난 후 움직인 차들을 식별하는 테스트

--- src/test/java/racingcar/utils/MoveTest.java
+++ src/test/java/racingcar/utils/MoveTest.java
@@ -14,44 +14,57 @@ import static racingcar.utils.StringUtilsTest.INPUT_STRING_TRUE_CASE;
 
 public class MoveTest {
     private static Move move;
+    private static ArrayList<Car> cars;
 
     @BeforeAll
     static void beforeAll() {
-         move = new Move();
+        move = new Move();
         INPUT_STRING_TRUE_CASE = new NamesInput("pobi,crong,honux");
+        cars = INPUT_STRING_TRUE_CASE.constructCars();
     }
+
     @DisplayName("값이 주어질 때 전진여부 결정하는 기능 테스트")
     @Test
-    void isCarMoveTest(){
+    void isCarMoveTest() {
         assertThat(move.isCarMove(3)).isFalse();
         assertThat(move.isCarMove(8)).isTrue();
     }
-
 
 
     @DisplayName("랜덤값이 주어질 때 올바른 값이 주어지는지 여부 테스트")
     @Test
     void validateRandomMoveNumberTest() {
 
-    int randomNumber = move.getRandomNumber(0,9);
-    assertThat(randomNumber>=0&&randomNumber<=9).isTrue();
+        int randomNumber = move.getRandomNumber(0, 9);
+        assertThat(randomNumber >= 0 && randomNumber <= 9).isTrue();
     }
-
 
 
     @DisplayName("한 횟수가 끝난 뒤 location이 변경되는지 여부 테스트")
     @Test
     void addLocationTest() {
-        ArrayList<Car> cars = INPUT_STRING_TRUE_CASE.constructCars();
+
         cars.get(0).proceedGameOnce();
-        assumingThat(cars.get(0).getRandomNumber()>=4, () -> {
-            System.out.println("randomnumber : "+cars.get(0).getRandomNumber()+"자동차는 움직여야합니다");
-            assertThat(cars.get(0).getDistance()==1).isTrue();
+        assumingThat(cars.get(0).getRandomNumber() >= 4, () -> {
+            System.out.println("randomnumber : " + cars.get(0).getRandomNumber() + "자동차는 움직여야합니다");
+            assertThat(cars.get(0).getDistance() == 1).isTrue();
         });
-        assumingThat(cars.get(0).getRandomNumber()<4, () -> {
-            System.out.println("randomnumber : "+cars.get(0).getRandomNumber()+"자동차는 움직이지 않습니다");
-            assertThat(cars.get(0).getDistance()==0).isTrue();
+        assumingThat(cars.get(0).getRandomNumber() < 4, () -> {
+            System.out.println("randomnumber : " + cars.get(0).getRandomNumber() + "자동차는 움직이지 않습니다");
+            assertThat(cars.get(0).getDistance() == 0).isTrue();
         });
 
+    }
+
+    @DisplayName("한 게임이 끝난 후 움직인 차들을 식별하는 테스트")
+    @Test
+    void pickMovedCars() {
+        for (Car car : cars)
+            car.proceedGameOnce();
+        assumingThat(cars.get(0).getRandomNumber() < 4, () -> {
+            System.out.println("randomnumber : " + cars.get(0).getRandomNumber() + "자동차는 움직이지 않습니다");
+            assertThat(cars.get(0).getDistance() == 0).isTrue();
+        });
+        move.getJustMovedCars(cars);
     }
 }
