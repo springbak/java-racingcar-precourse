ef7659f246a25fcd2370edf55587c62f37653490
springbak <bmpark0403@naver.com>
2022-10-07 21:24:47 +0900

    feat(StringUtils): 우승 자동차가 여러대일때 이름 사이에, 삽입하는 기능

--- src/main/java/racingcar/utils/StringUtils.java
+++ src/main/java/racingcar/utils/StringUtils.java
@@ -1,10 +1,20 @@
 package racingcar.utils;
-import java.util.Arrays;
+import racingcar.entities.Car;
+
+import java.util.ArrayList;
 public class StringUtils {
     public static final String SPLIT_EXPRESSION = ",";
     public String[] getNamesFromUserInput(String inputString) {
-        String[] s = inputString.split(SPLIT_EXPRESSION);
-        System.out.println(Arrays.toString(s));
         return inputString.split(SPLIT_EXPRESSION);
+    }
+
+    public String insertCommaBetweenWinnersName(ArrayList<Car> winnerCars) {
+        StringBuilder winnerCarsNameString = new StringBuilder();
+        winnerCarsNameString.append(winnerCars.remove(0).getName());
+        for(Car car :winnerCars)
+            winnerCarsNameString.append(", ").append(car.getName());
+
+        return winnerCarsNameString.toString();
+
     }
 }
