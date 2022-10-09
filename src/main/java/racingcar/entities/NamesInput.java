0f098e1c6032f465aa06287db2adb658b47272c0
springbak <bmpark0403@naver.com>
2022-10-09 20:44:03 +0900

    feat(NamesInput): 입력받은 이름 input validation

--- src/main/java/racingcar/entities/NamesInput.java
+++ src/main/java/racingcar/entities/NamesInput.java
@@ -3,25 +3,39 @@ package racingcar.entities;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class NamesInput {
 
     public static final String SPLIT_EXPRESSION = ",";
-    private final String[] names;
+    public static final int NAME_MAX_LENGTH= 5;
+    private String[] names;
 
     public NamesInput(String input) {
         try {
-            this.names = input.split(SPLIT_EXPRESSION);
-        } catch (IllegalArgumentException e) {
-            throw new IllegalArgumentException("[ERROR]입력값이 잘못 되었습니다. 이름 구분을 쉼표(,)로 입력해주세요");
+            try {
+                this.names = input.split(SPLIT_EXPRESSION);
+            } catch (IllegalArgumentException e) {
+                throw new IllegalArgumentException("[ERROR]입력값이 잘못 되었습니다. 이름 구분을 쉼표(,)로 입력해주세요");
+            }
+//        System.out.println(Arrays.toString(this.names));
+//        System.out.println("length : "+ this.names.length);
+            if (Arrays.equals(this.names, new String[]{}))
+                throw new IllegalArgumentException("[ERROR]입력값이 없습니다. 다시 입력해주세요");
+            if (this.names.length == 1)
+                throw new IllegalArgumentException("[ERROR]이름 구분을 쉼표(,)로 입력해주세요");
+            for (String name : this.names)
+                validateNameLength(name);
+        }catch (IllegalArgumentException e){
+            NamesInput namesInput = new NamesInput(readLine());
         }
-        System.out.println(Arrays.toString(this.names));
-        System.out.println("length : "+ this.names.length);
-        if(Arrays.equals(this.names,new String[]{}))
-            throw new IllegalArgumentException("[ERROR]입력값이 없습니다. 다시 입력해주세요");
-        if(this.names.length ==1)
-            throw new IllegalArgumentException("[ERROR]이름 구분을 쉼표(,)로 입력해주세요");
+    }
 
-
+    private void validateNameLength(String name) {
+        if(name.length()>NAME_MAX_LENGTH) {
+            System.out.println("[ERROR]이름 '" + name + "'은 길이가 5가 넘습니다. 다른 이름을 입력해주세요 ");
+            throw new IllegalArgumentException("[ERROR]이름 '" + name + "'은 길이가 5가 넘습니다. 다른 이름을 입력해주세요 ");
+        }
     }
 
     public String[] getNames() {
